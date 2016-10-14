012b49907ec12eac2f0a467065f18152805d1c18
kkayaomer <buraksezer1605@gmail.com>
2016-10-14 17:00:44 +0300

    index.php
    index.php

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Omer
+ * Date: 14.10.2016
+ * Time: 16:55
+ */
+include("fonksiyon/fonksiyon.php");
+?>
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" xmlns="http://www.w3.org/1999/html"/>
+<head>
+    <title>firsat.com </title>
+    <link rel="stylesheet" href="styles/style.css" media="all"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+</head>
+<body>
+<!--main container başlangıç yeri.--->
+<div class="main_wrapper">
+    <!--header  başlangıç yeri.--->
+    <div class="header_wrapper">
+        <div style="height: 140px;">
+            <a href="index.php"><img id="logo" src="resimler/logo.gif"/></a>
+            <img id="banner" src="resimler/banner.gif"/>
+        </div>
+    </div>
+    <!--header bitiş yeri.--->
+    <!--navigation bar başlangıç yeri.--->
+    <div class="menubar">
+        <ul id="menu">
+            <li><a href="index.php">Anasayfa</a></li>
+            <li><a href="urunler.php">Ürünler</a></li>
+            <li><a href="musteri/hesabım.php">Hesabım</a></li>
+            <li><a href="#">Kayd Ol</a></li>
+            <li><a href="sepet.php">Alışveriş Sepeti</a></li>
+            <li><a href="#">İletişim</a></li>
+        </ul>
+        <div id="form">
+            <form method="get" action="results.php" enctype="multipart/form-data">
+                <input type="text" name="user_query" placeholder="Bir ürün ara"/>
+                <input type="submit" name="search" value="search"/>
+            </form>
+        </div>
+    </div>
+    <!--navigation bar bitiş yeri.--->
+    <!--content wrapper başlangıç--->
+    <div class="content_wrapper">
+        <div id="sidebar">
+            <div class="sidebar-inner">
+                <div id="sidebar_title"> Kategoriler</div>
+                <ul id="kategori">
+                    <?php getKategori(); ?>
+                </ul>
+                <div id="sidebar_title"> Markalar</div>
+                <ul id="marka">
+                    <?php getMarka(); ?>
+                </ul>
+            </div>
+        </div>
+        <div id="content_area">
+            <?php Alisveris_Sepeti(); ?>
+            <div id="alisveris_sepeti">
+                <span style="float: right; font-size: 18px; padding: 5px; line-height:40px; ">Hoşgeldiniz!  <b
+                        style="color:yellow">
+                        Alışveriş Sepeti -</b>Toplam Adet: <?php Toplam_Adet(); ?>
+                    Toplam Fiyat: <?php Toplam_Fiyat(); ?>
+                    <a href="sepet.php" style="color: yellow">Sepete Git</a>
+                                             </span>
+            </div>
+            <?php echo $ip = getIp(); ?>
+            <div id="urun_box">
+                <?php getUrun(); ?>
+                <?php getUrunKategori(); ?>
+                <?php getUrunMarka(); ?>
+            </div>
+        </div>
+    </div>
+    <!--content wrapper bitiş--->
+    <div id="footer">
+        <h2 style="text-align: center; padding-top:20px; ">&copy; 2016 by www.firsat.com</h2>
+    </div>
+</div>
+<script type="text/javascript">
+    $(function () {
+        $('.content_wrapper').css('min-height', $('.sidebar-inner').height());
+    });
+</script>
+<!--main container bitiş yeri-->
+</body>
+</html>
